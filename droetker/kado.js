e81cc9875feafd7c57860fd7a16cb28cca6af3bb
obibramkenobi <bramvandijk92@gmail.com>
2017-07-17 16:04:23 +0200

    Add files via upload

--- droetker/kado.js
+++ droetker/kado.js
@@ -1,22 +1,12 @@
 var anim;
     var animData = {
-        container: document.getElementById('container400px'),
+        container: document.getElementById('container'),
         renderer: 'canvas',
         loop: false,
         prerender:true,
         autoplay: true,
-        path: 'kado700px.json'
+        path: 'kado.json'
     };
     anim = bodymovin.loadAnimation(animData);
 
 
-var anim;
-    var animData = {
-        container: document.getElementById('container700px'),
-        renderer: 'svg',
-        loop: false,
-        prerender:true,
-        autoplay: true,
-        path: 'kado400px.json'
-    };
-    anim = bodymovin.loadAnimation(animData);
